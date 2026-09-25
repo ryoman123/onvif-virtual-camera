89c324aedab128632a93a6190159b42b68b53a5b
ryoman123 <ryoman123@gmail.com>
2026-09-25 19:43:46 -0400

    test: cover service-specific capability consistency

---
+++ test/service-capabilities.test.js
@@ -0,0 +1,102 @@
+const test = require("node:test");
+const assert = require("node:assert/strict");
+
+const DeviceService = require("../src/services/device-service");
+const MediaService = require("../src/services/media-service");
+const {
+    buildDeviceServiceCapabilities,
+    buildMediaServiceCapabilities,
+    renderDeviceServiceCapabilitiesXml,
+    renderMediaServiceCapabilitiesXml
+} = require("../src/service-capabilities");
+
+global.runtime = { enable_debug_logs: false };
+
+function cameraFixture() {
+    return {
+        name: "Camera-Test",
+        mac: "02:00:00:00:00:80",
+        identity: {
+            manufacturer: "VirtualCam",
+            model: "Test Virtual Camera",
+            firmwareVersion: "1.0",
+            serialNumber: "020000000080",
+            hardwareId: "Test Virtual Camera-020000000080"
+        },
+        endpoints: {
+            deviceServiceUrl: "http://192.0.2.80/onvif/device_service",
+            mediaServiceUrl: "http://192.0.2.80/onvif/media_service",
+            rtspUriHq: "rtsp://192.0.2.80:8554/main",
+            rtspUriLq: "rtsp://192.0.2.80:8554/sub",
+            snapshotUri: "http://192.0.2.80/snapshot.jpg"
+        },
+        streams: {
+            hq: {
+                encoding: "H265",
+                width: 3840,
+                height: 2160,
+                framerate: 15,
+                bitrate: 4096,
+                quality: 5
+            },
+            lq: {
+                encoding: "H265",
+                width: 960,
+                height: 480,
+                framerate: 15,
+                bitrate: 1024,
+                quality: 3
+            }
+        }
+    };
+}
+
+test("Media GetServiceCapabilities returns the shared service-specific payload", async () => {
+    const media = new MediaService(cameraFixture());
+    const response = await media.GetServiceCapabilities();
+
+    assert.deepEqual(response.Capabilities, buildMediaServiceCapabilities());
+    assert.equal(response.Capabilities.$attributes.SnapshotUri, true);
+    assert.equal(
+        response.Capabilities.ProfileCapabilities.$attributes.MaximumNumberOfProfiles,
+        2
+    );
+    assert.equal(
+        response.Capabilities.StreamingCapabilities.$attributes.RTPMulticast,
+        false
+    );
+    assert.equal(
+        response.Capabilities.StreamingCapabilities.$attributes.RTP_RTSP_TCP,
+        true
+    );
+});
+
+test("GetServices omits service capabilities unless requested", async () => {
+    const device = new DeviceService(cameraFixture());
+    const response = await device.GetServices({ IncludeCapability: false });
+
+    assert.equal(response.Service.length, 2);
+    assert.equal(Object.hasOwn(response.Service[0], "Capabilities"), false);
+    assert.equal(Object.hasOwn(response.Service[1], "Capabilities"), false);
+});
+
+test("GetServices includes namespaced Device and Media service capabilities", async () => {
+    const device = new DeviceService(cameraFixture());
+    const response = await device.GetServices({ IncludeCapability: true });
+
+    const deviceXml = response.Service[0].Capabilities.$xml;
+    const mediaXml = response.Service[1].Capabilities.$xml;
+
+    assert.equal(deviceXml, renderDeviceServiceCapabilitiesXml(buildDeviceServiceCapabilities()));
+    assert.equal(mediaXml, renderMediaServiceCapabilitiesXml(buildMediaServiceCapabilities()));
+
+    assert.match(deviceXml, /<tds:Capabilities[^>]*xmlns:tds="http:\/\/www\.onvif\.org\/ver10\/device\/wsdl"/);
+    assert.match(deviceXml, /DiscoveryResolve="true"/);
+    assert.match(deviceXml, /DiscoveryBye="true"/);
+
+    assert.match(mediaXml, /<trt:Capabilities[^>]*xmlns:trt="http:\/\/www\.onvif\.org\/ver10\/media\/wsdl"/);
+    assert.match(mediaXml, /SnapshotUri="true"/);
+    assert.match(mediaXml, /MaximumNumberOfProfiles="2"/);
+    assert.match(mediaXml, /RTPMulticast="false"/);
+    assert.match(mediaXml, /RTP_RTSP_TCP="true"/);
+});
