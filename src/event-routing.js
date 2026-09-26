1c6219a5988cdc707be6d676c3d90ee4e5782aec
ryoman123 <ryoman123@gmail.com>
2026-09-26 16:02:40 -0400

    events: add dynamic PullPoint request routing

---
+++ src/event-routing.js
@@ -0,0 +1,57 @@
+const EVENT_SERVICE_PATH = "/onvif/event_service";
+const PULLPOINT_SERVICE_PATH = "/onvif/event_service/subscriptions";
+
+function pathnameFromUrl(value) {
+    try {
+        return new URL(value || "/", "http://localhost").pathname;
+    } catch (_) {
+        return "/";
+    }
+}
+
+function subscriptionIdFromUrl(value) {
+    const pathname = pathnameFromUrl(value);
+    const match = pathname.match(
+        /^\/onvif\/event_service\/subscriptions\/([^/]+)$/
+    );
+
+    if (!match) {
+        return null;
+    }
+
+    try {
+        return decodeURIComponent(match[1]);
+    } catch (_) {
+        return null;
+    }
+}
+
+function rewritePullPointRequest(req) {
+    const id = subscriptionIdFromUrl(req && req.url);
+    if (!id) {
+        return false;
+    }
+
+    req.onvifSubscriptionId = id;
+    req.onvifOriginalUrl = req.url;
+    req.url = PULLPOINT_SERVICE_PATH;
+
+    return true;
+}
+
+function isEventServiceRequest(value) {
+    const pathname = pathnameFromUrl(value);
+
+    return pathname === EVENT_SERVICE_PATH
+        || pathname === PULLPOINT_SERVICE_PATH
+        || pathname.startsWith(PULLPOINT_SERVICE_PATH + "/");
+}
+
+module.exports = {
+    EVENT_SERVICE_PATH,
+    PULLPOINT_SERVICE_PATH,
+    pathnameFromUrl,
+    subscriptionIdFromUrl,
+    rewritePullPointRequest,
+    isEventServiceRequest
+};
