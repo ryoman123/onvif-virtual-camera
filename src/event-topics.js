a590fd2ba2fbd8a5bf0c3c1b1bf98eed0dad7048
ryoman123 <ryoman123@gmail.com>
2026-09-26 15:53:13 -0400

    events: describe ONVIF detection topic payloads

--- src/event-topics.js
+++ src/event-topics.js
@@ -6,9 +6,103 @@ const TOPICS = Object.freeze({
     PACKAGE: "UserAlarm/IVA/PackageDetect"
 });
 
-const DEFAULT_TOPICS = Object.freeze(Object.values(TOPICS));
+const TOPIC_DEFINITIONS = Object.freeze([
+    Object.freeze({
+        key: "MOTION",
+        topic: TOPICS.MOTION,
+        isProperty: true,
+        source: Object.freeze([
+            Object.freeze({
+                name: "VideoSourceConfigurationToken",
+                type: "tt:ReferenceToken"
+            })
+        ]),
+        data: Object.freeze([
+            Object.freeze({
+                name: "IsMotion",
+                type: "xs:boolean"
+            })
+        ])
+    }),
+    Object.freeze({
+        key: "PERSON",
+        topic: TOPICS.PERSON,
+        isProperty: true,
+        source: Object.freeze([
+            Object.freeze({
+                name: "VideoSourceConfigurationToken",
+                type: "tt:ReferenceToken"
+            })
+        ]),
+        data: Object.freeze([
+            Object.freeze({
+                name: "State",
+                type: "xs:boolean"
+            })
+        ])
+    }),
+    Object.freeze({
+        key: "VEHICLE",
+        topic: TOPICS.VEHICLE,
+        isProperty: true,
+        source: Object.freeze([
+            Object.freeze({
+                name: "VideoSourceConfigurationToken",
+                type: "tt:ReferenceToken"
+            })
+        ]),
+        data: Object.freeze([
+            Object.freeze({
+                name: "State",
+                type: "xs:boolean"
+            })
+        ])
+    }),
+    Object.freeze({
+        key: "ANIMAL",
+        topic: TOPICS.ANIMAL,
+        isProperty: true,
+        source: Object.freeze([
+            Object.freeze({
+                name: "VideoSourceConfigurationToken",
+                type: "tt:ReferenceToken"
+            })
+        ]),
+        data: Object.freeze([
+            Object.freeze({
+                name: "State",
+                type: "xs:boolean"
+            })
+        ])
+    }),
+    Object.freeze({
+        key: "PACKAGE",
+        topic: TOPICS.PACKAGE,
+        isProperty: true,
+        source: Object.freeze([
+            Object.freeze({
+                name: "VideoSourceConfigurationToken",
+                type: "tt:ReferenceToken"
+            })
+        ]),
+        data: Object.freeze([
+            Object.freeze({
+                name: "State",
+                type: "xs:boolean"
+            })
+        ])
+    })
+]);
+
+const DEFAULT_TOPICS = Object.freeze(TOPIC_DEFINITIONS.map((definition) => definition.topic));
+
+function getTopicDefinition(topic) {
+    return TOPIC_DEFINITIONS.find((definition) => definition.topic === topic) || null;
+}
 
 module.exports = {
     TOPICS,
-    DEFAULT_TOPICS
+    TOPIC_DEFINITIONS,
+    DEFAULT_TOPICS,
+    getTopicDefinition
 };
