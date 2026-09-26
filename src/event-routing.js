6f48b472df58457055d33e190e7d8f5ec1975488
ryoman123 <ryoman123@gmail.com>
2026-09-26 16:08:52 -0400

    events: match dynamic PullPoint URLs natively

--- src/event-routing.js
+++ src/event-routing.js
@@ -1,5 +1,6 @@
 const EVENT_SERVICE_PATH = "/onvif/event_service";
 const PULLPOINT_SERVICE_PATH = "/onvif/event_service/subscriptions";
+const PULLPOINT_SERVICE_PATTERN = /^\\/onvif\\/event_service\\/subscriptions\\/[^/]+\\/?$/;
 
 function pathnameFromUrl(value) {
     try {
@@ -50,6 +51,7 @@ function isEventServiceRequest(value) {
 module.exports = {
     EVENT_SERVICE_PATH,
     PULLPOINT_SERVICE_PATH,
+    PULLPOINT_SERVICE_PATTERN,
     pathnameFromUrl,
     subscriptionIdFromUrl,
     rewritePullPointRequest,
